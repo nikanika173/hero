2faf364dbd273f6b67e1b58368348b45c87e6b16
nikanika173 <147316302+nikanika173@users.noreply.github.com>
2025-04-02 11:35:38 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import './reset.scss'
+
+const App = () => {
+  return (
+    <div className="App">
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "../modules/HeroSection.module.scss";
+import heroImage from "../assets/Hero-Img.png";
+
+const HeroSection = () => {
+  return (
+    <section className={styles.heroSection}>
+      <div className={`${styles.heroSectionWrapper} container`}>
+        <div className={styles.heroContentWrapper}>
+          <p className={styles.heroFirstLine}>Pro.Beyond.</p>
+          <h1 className={styles.heroTitle}>
+            iPhone 14 <span>Pro</span>
+          </h1>
+          <p className={styles.heroThirdLine}>
+            Created to change everything for the better. For everyone
+          </p>
+          <div className={styles.heroBtnWrapper}>
+            <button className={styles.whiteBtnLarge}>Shop Now</button>
+          </div>
+        </div>
+
+        <figure className={styles.heroImgWrapper}>
+          <img src={heroImage} alt="mobile-phone" className={styles.heroImg} />
+        </figure>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
